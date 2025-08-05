17175e7ed5e03f11b9fc107dac09949c1edc6648
airmanrizzo-commits <airmanrizzo@gmail.com>
2025-08-05 18:56:15 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,33 +1,36 @@
 export default {
-  expo: {
-    name: "Watchman App",
-    slug: "watchman-app",
-    version: "1.0.0",
-    orientation: "portrait",
-    jsEngine: "hermes",
-    icon: "./assets/icon.png",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
+  name: "Watchman App",
+  slug: "watchman-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  jsEngine: "hermes",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#000000"
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.firegate.watchman"
+  },
+  android: {
+    package: "com.firegate.watchman",
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
       backgroundColor: "#000000"
-    },
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.firegate.watchman"
-    },
-    android: {
-      package: "com.firegate.watchman",
-      adaptiveIcon: {
-        foregroundImage: "./assets/icon.png",
-        backgroundColor: "#000000"
-      }
-    },
-    web: {
-      favicon: "./assets/icon.png"
+    }
+  },
+  web: {
+    favicon: "./assets/icon.png"
+  },
+  extra: {
+    eas: {
+      projectId: "bf2c35e0"
     }
   }
-}
+};
