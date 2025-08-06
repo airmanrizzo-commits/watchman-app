9abdcca5533ec734344a75b46ef4aeb2a94122b4
airmanrizzo-commits <airmanrizzo@gmail.com>
2025-08-06 13:16:27 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,30 +1,24 @@
-export default {
-  expo: {
-    name: "watchman-app",
-    slug: "watchman-app",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.firegate.watchman",
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false
+{
+  "cli": {
+    "version": ">= 3.13.0"
+  },
+  "build": {
+    "production": {
+      "ios": {
+        "image": "macos-xcode16",
+        "buildType": "release",
+        "developmentClient": false
       }
-    },
-    extra: {
-      eas: {
-        projectId: "bf2c35e0-377d-4a5f-8157-86e09abc949f"
+    }
+  },
+  "submit": {
+    "production": {
+      "ios": {
+        "ascAppId": "6749645144", 
+        "appleId": "airmanrizzo@gmail.com",
+        "appleTeamId": "9295GJRZLK",
+        "companyName": "FireGate Bioscience LLC"
       }
     }
   }
-};
+}
